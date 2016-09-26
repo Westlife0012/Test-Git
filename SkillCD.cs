e35a8cebb6aff0431aaf504bd58dd6d0194c9840
Westlife0012 <827125604@qq.com>
2016-09-26 10:18:15 +0800

    File
    filetest

---
+++ SkillCD.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkillCD : MonoBehaviour {
+	private float currentTime;
+	public float SkillTime1;
+	private float hasTime;
+	private bool isCanAttack=true;
+	public UISprite mySp;
+	public UILabel myLab;
+
+	// Use this for initialization
+	void Start () {
+		currentTime = 0;
+		SkillTime1 = 5;
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (mySp.fillAmount.Equals(1)) {
+			return;
+		}
+		currentTime += Time.deltaTime;
+		hasTime = SkillTime1 - Mathf.FloorToInt (currentTime);
+		myLab.text = hasTime.ToString ();
+		mySp.fillAmount = currentTime / SkillTime1;
+		print (mySp.fillAmount);
+		if (currentTime>=SkillTime1) {
+			currentTime = 0;
+			isCanAttack = true;
+			myLab.enabled = false;
+			this.GetComponent<UIButton> ().enabled = true;
+		}
+	}
+
+	void OnClick(){
+		if (isCanAttack) {
+			print (isCanAttack);
+			mySp.fillAmount = 0;
+			isCanAttack = false;
+			myLab.enabled = true;
+			this.GetComponent<UIButton> ().enabled = false;
+		}
+
+	}
+}
---
+++ StartScene.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScene : MonoBehaviour {
+
+	public MovieTexture myMovie;
+	private AudioSource myAudio;
+	private bool isPlay=true;
+	// Use this for initialization
+	void Start () {
+		myMovie.Play ();
+		myMovie.loop = true;
+		myAudio =this.GetComponent<AudioSource> ();
+		myAudio.Play ();
+		myAudio.loop = true;
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	
+	public void movieJump(){
+		Application.LoadLevel ("SkillCD");
+	}
+	public void moviePause()
+	{
+		if (isPlay) {
+			myMovie.Pause ();
+			myAudio.Pause ();
+			isPlay = false;
+		} else {
+			myMovie.Play ();
+			myAudio.Play ();
+			isPlay = true;
+		}
+	}
+}
